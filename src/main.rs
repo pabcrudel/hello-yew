2e9af44856d75490b7acb3366a762bbdb4c79f7c
pabcrudel <pabcrudel@gmail.com>
2024-01-21 17:27:02 +0100

    change: Remove comments

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,17 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-  // Similar to `useState()` React Hook
   let counter: UseStateHandle<i32> = use_state(|| 0);
 
-  // `onclick` callback function
   let onclick = {
     let counter: UseStateHandle<i32> = counter.clone();
 
-    /*
-      This section doesn't ends with semicolon because this functions
-      returns it
-    */
     move |_| {
       let value: i32 = *counter + 1;
       counter.set(value);
     }
   };
 
-  // Like in React, this component returns an HTML
   html! {
     <>
       <header>
@@ -28,8 +21,6 @@ fn App() -> Html {
 
       <main>
         <div>
-          // {onclick} is a shorthand of: onclick={onclick}
-          // That is done because the callback fn has the same name
           <button {onclick}>{ "+1" }</button>
           <p>{ *counter }</p>
         </div>
